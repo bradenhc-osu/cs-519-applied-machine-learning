531160c80f8861607edff2e568e77c765d2a5d2f
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-04-06 12:51:59 -0700

    updating binarization, default to not include numerical features

--- homework-1/preprocess.py
+++ homework-1/preprocess.py
@@ -2,6 +2,10 @@ import numpy as np
 from os import path
 
 
+__age_feature_index = 0
+__hours_worked_feature_index = 7
+
+
 def fetch_data(filename):
     data = []
     with open(filename, "r") as f:
@@ -11,7 +15,7 @@ def fetch_data(filename):
     return data
 
 
-def preprocess_data(filename):
+def preprocess_data(filename, binarize_numerical=False):
 
     data = fetch_data(filename)
 
@@ -23,19 +27,29 @@ def preprocess_data(filename):
                 return 1
             else:
                 return -1
+
+        elif not binarize_numerical and feature_index == __age_feature_index:
+            return int(feature_value)
+
+        elif not binarize_numerical and feature_index == __hours_worked_feature_index:
+            return int(feature_value)
+
         else:
             f = (feature_index, feature_value)
             if f not in mappings:
                 mappings[f] = len(mappings)
             return mappings[f]
 
-    mapped = [[map_data(i, j, v) for j, v in enumerate(row)] for i, row in enumerate(data)]
+    mapped = [[map_data(i, j, v) for j, v in enumerate(row)]
+              for i, row in enumerate(data)]
 
     bindata = np.zeros((len(data), len(mappings) + 1))
     for i, row in enumerate(mapped):
         for j, v in enumerate(row):
             if j == len(row) - 1:
                 bindata[i][-1] = v
+            elif not binarize_numerical and (j == __age_feature_index or j == __hours_worked_feature_index):
+                bindata[i][j] = v
             else:
                 bindata[i][v] = 1
 
