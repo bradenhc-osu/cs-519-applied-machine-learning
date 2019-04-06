c995e1d386bd683b2a29469e912ad29f51472530
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-04-06 08:57:03 -0700

    implementing binarization in preprocessing step

--- homework-1/preprocess.py
+++ homework-1/preprocess.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from os import path
 
 
@@ -10,14 +10,41 @@ def fetch_data(filename):
             data.append(values)
     return data
 
+
 def preprocess_data(filename):
-    # Fetch the string data from the file
+
     data = fetch_data(filename)
-    print(data)
-    return None
+
+    mappings = {}
+
+    def map_data(row_index, feature_index, feature_value):
+        if feature_index == len(data[0]) - 1:
+            if feature_value == ">50K":
+                return 1
+            else:
+                return -1
+        else:
+            f = (feature_index, feature_value)
+            if f not in mappings:
+                mappings[f] = len(mappings)
+            return mappings[f]
+
+    mapped = [[map_data(i, j, v) for j, v in enumerate(row)] for i, row in enumerate(data)]
+
+    bindata = np.zeros((len(data), len(mappings) + 1))
+    for i, row in enumerate(mapped):
+        for j, v in enumerate(row):
+            if j == len(row) - 1:
+                bindata[i][-1] = v
+            else:
+                bindata[i][v] = 1
+
+    return bindata
 
 
 if __name__ == '__main__':
     # Preprocess the training data
     this_dir = path.dirname(path.realpath(__file__))
-    preprocess_data(path.join(this_dir, "data", "income.train.txt.5k"))
+    preprocessed_data = preprocess_data(
+        path.join(this_dir, "data", "income.train.txt.5k"))
+    print(preprocessed_data[500])
